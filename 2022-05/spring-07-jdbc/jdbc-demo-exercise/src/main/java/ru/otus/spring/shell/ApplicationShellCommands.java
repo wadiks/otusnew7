2f390d297ce9d12ba683113ecc12e3941a8731c6
wadiks <33803217+wadiks@users.noreply.github.com>
2022-08-01 16:46:30 +0300

    Задание Jdbc. Доработал контекст.

--- 2022-05/spring-07-jdbc/jdbc-demo-exercise/src/main/java/ru/otus/spring/shell/ApplicationShellCommands.java
+++ 2022-05/spring-07-jdbc/jdbc-demo-exercise/src/main/java/ru/otus/spring/shell/ApplicationShellCommands.java
@@ -1,6 +1,7 @@
 package ru.otus.spring.shell;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
@@ -10,7 +11,6 @@ import ru.otus.spring.dao.GenreDao;
 import ru.otus.spring.model.Authors;
 import ru.otus.spring.model.Books;
 import ru.otus.spring.model.Genre;
-import ru.otus.spring.sourse.ApplicationContextHolder;
 
 import java.util.List;
 import java.util.Scanner;
@@ -19,6 +19,17 @@ import java.util.stream.Collectors;
 @ShellComponent
 @RequiredArgsConstructor
 public class ApplicationShellCommands {
+
+    private BooksDao booksDao;
+    private AuthorsDao authorsDao;
+    private GenreDao genreDao;
+
+    @Autowired
+    public ApplicationShellCommands(BooksDao booksDao, AuthorsDao authorsDao, GenreDao genreDao) {
+        this.booksDao = booksDao;
+        this.authorsDao = authorsDao;
+        this.genreDao = genreDao;
+    }
 
     @ShellMethod(value = "Help command", key = {"?"})
     public void help() {
@@ -41,78 +52,66 @@ public class ApplicationShellCommands {
 
     @ShellMethod(value = "Количество книг", key = {"c", "count"})
     public void count() {
-        var books = context().getBean(BooksDao.class);
-        System.out.println("Количество книг = " + books.count());
+        System.out.println("Количество книг = " + booksDao.count());
     }
 
     @ShellMethod(value = "Вывод всех книг", key = {"books", "bGetAll"})
     public void getAll() {
-        var books = context().getBean(BooksDao.class);
-        var authors = context().getBean(AuthorsDao.class);
-        var genre = context().getBean(GenreDao.class);
-        bPrint(books.getAll(), authors.getAll(), genre.getAll());
+        bPrint(booksDao.getAll(), authorsDao.getAll(), genreDao.getAll());
     }
 
     @ShellMethod(value = "Найти книгу по id", key = {"bId", "bGetId"})
     public void getBookById() {
-        var books = context().getBean(BooksDao.class);
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите номер книги:");
         int number = sc.nextInt();
-        var book = books.getById(number);
+        var book = booksDao.getById(number);
         System.out.println(String.format("Номер книги = %s Наименование книги = %s", book.getId(), book.getName()));
     }
 
     @ShellMethod(value = "Удалить книгу", key = {"del", "delete"})
     public void delBook() {
-        var books = context().getBean(BooksDao.class);
         System.out.println("Какую книгу хотите удалить");
-        bPrint(books.getAll());
+        bPrint(booksDao.getAll());
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите номер книги:");
         int number = sc.nextInt();
-        books.deleteById(number);
+        booksDao.deleteById(number);
         System.out.println("Книга удалена");
     }
 
     @ShellMethod(value = "Изменить название книги", key = {"rename"})
     public void updateBook() {
-        var books = context().getBean(BooksDao.class);
-        var authors = context().getBean(AuthorsDao.class);
-        var genre = context().getBean(GenreDao.class);
         System.out.println("Какую книгу хотите изменить");
-        bPrint(books.getAll());
+        bPrint(booksDao.getAll());
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите номер книги:");
         int number = sc.nextInt();
         System.out.println("Введите название книги:");
         sc.nextLine();
         String name = sc.nextLine();
-        gPrint(genre.getAll());
+        gPrint(genreDao.getAll());
         System.out.println("Введите номер жанра:");
         int gNumber = sc.nextInt();
-        aPrint(authors.getAll());
+        aPrint(authorsDao.getAll());
         System.out.println("Введите номер автора:");
         int aNumber = sc.nextInt();
-        books.update(new Books(number, name, gNumber, aNumber));
+        booksDao.update(new Books(number, name, gNumber, aNumber));
         System.out.println("Книга изменена");
     }
 
     @ShellMethod(value = "Добавить книгу", key = {"ins", "insert"})
     public void insertBook() {
-        var books = context().getBean(BooksDao.class);
-        var authors = context().getBean(AuthorsDao.class);
-        var genre = context().getBean(GenreDao.class);
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите название книги:");
         String name = sc.nextLine();
-        gPrint(genre.getAll());
+        gPrint(genreDao.getAll());
         System.out.println("Введите номер жанра:");
         int gNumber = sc.nextInt();
-        aPrint(authors.getAll());
+        aPrint(authorsDao.getAll());
         System.out.println("Введите номер автора:");
         int aNumber = sc.nextInt();
-        books.insert(new Books(name, gNumber, aNumber));
+        booksDao.insert(new Books(name, gNumber, aNumber));
         System.out.println("Книга добавлена");
     }
 
@@ -120,24 +119,21 @@ public class ApplicationShellCommands {
 
     @ShellMethod(value = "Посмотреть всех аторов", key = {"authors", "aGetAll"})
     public void getAutors() {
-        var autors = context().getBean(AuthorsDao.class);
         System.out.println("Выведены все авторы книг = ");
-        aPrint(autors.getAll());
+        aPrint(authorsDao.getAll());
     }
 
     @ShellMethod(value = "Количество авторов", key = {"a", "aCount"})
     public void getAutorsCount() {
-        var autors = context().getBean(AuthorsDao.class);
-        System.out.println("Количествол авторов = " + autors.count());
+        System.out.println("Количествол авторов = " + authorsDao.count());
     }
 
     @ShellMethod(value = "Найти автора по id", key = {"aId", "aGetId"})
     public void getAutorsGetId() {
-        var autors = context().getBean(AuthorsDao.class);
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите номер автора:");
         int number = sc.nextInt();
-        var author = autors.getById(number);
+        var author = authorsDao.getById(number);
         System.out.println(String.format("Номер автора = %s Назвавние автора = %s %s", author.getId(), author.getName(), author.getSurname()));
     }
 
@@ -145,33 +141,24 @@ public class ApplicationShellCommands {
 
     @ShellMethod(value = "Посмотреть все жанры", key = {"genres", "gGetAll"})
     public void getGenre() {
-        var genre = context().getBean(GenreDao.class);
         System.out.println("Все жанры книг = ");
-        gPrint(genre.getAll());
+        gPrint(genreDao.getAll());
     }
 
     @ShellMethod(value = "Количество жанров", key = {"g", "gCount"})
     public void getGenreCount() {
-        var genre = context().getBean(GenreDao.class);
-        System.out.println("Количество жанров книг = " + genre.count());
+        System.out.println("Количество жанров книг = " + genreDao.count());
     }
 
     @ShellMethod(value = "Найти жанр по id", key = {"gId", "gGetId"})
     public void getGenreGetId() {
-        var genre = context().getBean(GenreDao.class);
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите номер жанра:");
         int number = sc.nextInt();
-        var eGenre = genre.getById(number);
+        var eGenre = genreDao.getById(number);
         System.out.println(String.format("Номер жанра = %s Наименование жанра = %s", eGenre.getId(), eGenre.getName()));
     }
 
-
-    public ApplicationContext context() {
-        var ctx = ApplicationContextHolder.getApplicationContext();
-        if (null == ctx) System.out.println("Не загрузился контекст");
-        return ctx;
-    }
 
     private void bPrint(List<Books> books) {
         books.forEach(b -> {
@@ -202,7 +189,6 @@ public class ApplicationShellCommands {
             System.out.println(String.format("Номер жанра = %s Наименование жанра = %s", g.getId(), g.getName()));
         });
     }
-
 }
 
 
