ea0656968f4e40fc72deb101f32b5f696e7513a9
wadiks <33803217+wadiks@users.noreply.github.com>
2022-09-23 13:39:10 +0300

    ДЗ Security.

--- 2022-05/spring-07-jdbc/jdbc-demo-exercise/src/main/java/ru/otus/spring/dto/BookDto.java
+++ 2022-05/spring-07-jdbc/jdbc-demo-exercise/src/main/java/ru/otus/spring/dto/BookDto.java
@@ -1,40 +1,38 @@
 package ru.otus.spring.dto;
 
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
-
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import ru.otus.spring.model.Authors;
 import ru.otus.spring.model.Books;
 import ru.otus.spring.model.Genre;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @NoArgsConstructor
 @Data
 public class BookDto {
+
     public Long id;
-    public @NotBlank @Size(
-            min = 2,
-            max = 10
-    ) String name;
-    public @Size(
-            min = 2,
-            max = 200
-    ) String genres;
+
+    @NotBlank
+    @Size(min = 2,max = 10)
+    public String name;
+
+    @Size(min = 2,max = 200)
+    public String genres;
+
     private String authors;
 
     public List<Genre> setGenreFromString(String genre) {
-        return (List) Stream.of(genre.split(",")).map((m) -> {
-            return m.trim();
-        }).map(Genre::new).collect(Collectors.toList());
+        return Stream.of(genre.split(",")).map((m) -> m.trim()).map(Genre::new).collect(Collectors.toList());
     }
 
     public List<Authors> setAuthorsFromString(String authors) {
-        return (List) Stream.of(authors.split(",")).map((m) -> {
+        return Stream.of(authors.split(",")).map((m) -> {
             String[] array = m.trim().split(" ");
             return null != array[0] && !array[0].isEmpty() && null != array[1] && !array[1].isEmpty() ? new Authors(array[0], array[1]) : null;
         }).collect(Collectors.toList());
--- 2022-05/spring-07-jdbc/jdbc-demo-exercise/src/main/java/ru/otus/spring/security/SecurityConfiguration.java
+++ 2022-05/spring-07-jdbc/jdbc-demo-exercise/src/main/java/ru/otus/spring/security/SecurityConfiguration.java
@@ -1,18 +1,18 @@
 package ru.otus.spring.security;
 
-import java.util.List;
-import java.util.Properties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import ru.otus.spring.dao.SecurityDao;
 import ru.otus.spring.model.Security;
+
+import java.util.List;
+import java.util.Properties;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -23,7 +23,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     public void configure(HttpSecurity http) throws Exception {
-        ((HttpSecurity)((ExpressionUrlAuthorizationConfigurer.AuthorizedUrl)((HttpSecurity)((ExpressionUrlAuthorizationConfigurer.AuthorizedUrl)((HttpSecurity)http.csrf().disable()).authorizeRequests().antMatchers(new String[]{"/"})).anonymous().and()).authorizeRequests().antMatchers(new String[]{"/insert", "/edit", "/bookDelete"})).authenticated().and()).formLogin();
+        http.csrf().disable()
+                .authorizeRequests().antMatchers("/").anonymous()
+                .and().authorizeRequests().antMatchers("/insert", "/edit", "/bookDelete").authenticated()
+                .and()
+                .formLogin();
     }
 
     @Bean
@@ -39,7 +43,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
         Properties users = new Properties();
         List<Security> sec = this.securityDao.findAll();
-        sec.forEach(e ->  users.put(e.getUser(), e.getPassword() + "," + e.getRole() + ",enabled"));
+        sec.forEach(e -> users.put(e.getUser(), e.getPassword() + "," + e.getRole() + ",enabled"));
         return new InMemoryUserDetailsManager(users);
     }
 }
